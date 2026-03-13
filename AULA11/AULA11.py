f2c1b598d28b51c9934f77d3ad925924e342fdfc
evtonluis615@gmail.com <103472691+Luiis-dev@users.noreply.github.com>
2026-03-13 14:46:22 -0300

    ultimo do mundo01

---
+++ AULA11/AULA11.py
@@ -0,0 +1,46 @@
+print('\033[1;31;43mOlá, Mundo!')
+
+
+# OU
+
+print('\033[1;31;43mOlá, Mundo!\033[m')
+
+# OU
+
+print('\033[4;30;45mOlá, Mundo!\033[m')
+
+#
+
+print('\033[37mOlá, Mundo!\033[m')
+
+#
+
+print('\033[37mOlá, Mundo!\033[m')
+
+#
+
+print('\033[30mOlá, Mundo!\033[m')
+
+#
+
+print('\033[7;30mOlá, Mundo!\033[m')
+
+#
+
+a = 3
+b = 5
+print('Os valores são \033[32;44m{}\033[m e \033[32;44m{}\033[m!'.format(a, b))
+
+#
+
+nome = 'LUIS'
+print('Olá! muito prazer em te conhecer, {}{}!!'.format('\033[4;34m', nome, '\033[m'))
+
+#
+
+nome = 'LUIS'
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m',}
+print('Olá! muito prazer em te conhecer, {}{}!!'.format(cores['pretoebranco'], nome, cores ['\033[m']))
--- ex034.py
+++ ex034.py
@@ -1,1 +1,7 @@
-salário = float(input())
+salário = float(input('Qual é o salário do funcionário? R$ '))
+if salário <= 1250:
+    novo = salário + (salário * 15 / 100)
+else:
+    novo = salário + (salário * 10 / 100)
+print('Quem ganhava R$(:.2f) passe a ganhar R${:.2f} agora'.format(salário, novo))
+
