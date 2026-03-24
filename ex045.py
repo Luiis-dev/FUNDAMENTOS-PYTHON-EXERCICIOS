7dedee3d38425415f04936c7fe80da214ed1ff97
evtonluis615@gmail.com <103472691+Luiis-dev@users.noreply.github.com>
2026-03-24 18:20:18 -0300

    F-str aplicando

--- ex045.py
+++ ex045.py
@@ -1,4 +1,8 @@
 import  random
+
+# EM VEZ DE f'texto {variavel}', USA VÍRGULA NO PRINT
+# print("Olá", nome)  →  MAIS FACÍL DE ENTENDER NO INICIO
+
 print('-=-' * 20)
 print('JOKENPÔ')
 print('-=-' * 20)
@@ -25,3 +29,39 @@ elif (jogador == 1 and computador == 3) or \
     print('VOCÊ VENCEU!')
 else:
     print('COMPUTADOR VENCEU!')
+
+import random
+
+print('-=-' * 20)
+print('JOKENPÔ')
+print('-=-' * 20)
+print('1 - Pedra')
+print('2 - Papel')
+print('3 - Tesoura')
+
+jogador = int(input('Digite sua escolha: '))
+computador = random.randint(1, 3)
+
+#f-string: VARIÁVEL DERETO DENTRO DO {}
+print(f'Você escolheu: {jogador}')
+
+if computador == 1:
+    nome_pc = 'PEDRA'
+elif computador == 2:
+    nome_pc = 'PAPEL'
+else:
+    nome_pc = 'TESOURA'
+
+# PODE COLOCAR DUAS VARIÁVEIS NA MESMA LINHA
+print(f'Você: {jogador} | computador: {nome_pc}')
+
+if jogador == computador:
+    print('EMPATE!')
+elif (jogador == 1 and computador == 3) or \
+     (jogador == 3 and computador == 2) or \
+     (jogador == 2 and computador == 1):
+    print('Você VENCEU!')
+else:
+    print('COMPUTADOR VENCEU!')
+
+
